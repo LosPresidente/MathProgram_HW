779d83526291c332ae6bd74d459fd4d667ff76e4
LosPresidente <ibilek1@buffs.wtamu.edu>
2018-03-03 21:57:12 -0600

    idk

---
+++ MathWizardTestInClass/UnitTest1.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+
+using MathWizard.Controllers;
+using MathWizard.Models;
+
+
+namespace MathWizardTestInClass
+{
+    public class UnitTest1
+    {
+
+        MathController controller = new MathController(); 
+
+        [Fact]
+        public void Test1()
+        {
+
+            MathOp op = new MathOp();
+            //Arrange, create the input
+
+            //Act, chew the data
+
+            //Assert, result
+
+        }
+    }
+}
